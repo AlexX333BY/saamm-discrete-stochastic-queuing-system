6271f8adf66e137662156ca30baf6165bb42878e
Aliaksei <alexx333by@protonmail.com>
2019-09-28 16:14:21 +0300

    Jobs storage class implemented

---
+++ QueuingSystemLib/jobs_storage.cpp
@@ -0,0 +1,16 @@
+#include "jobs_storage.h"
+
+using namespace queuing_system;
+
+jobs_storage::jobs_storage() : jobs()
+{ }
+
+const std::vector<std::shared_ptr<job>>& jobs_storage::get_jobs() const
+{
+    return jobs;
+}
+
+void jobs_storage::store_job(const std::shared_ptr<job> &job)
+{
+    jobs.push_back(job);
+}
